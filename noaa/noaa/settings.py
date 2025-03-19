623dbf9523233d9a72fa07ced32f759a0cefdbe8
Eruis2579 <eruis2579@gmail.com>
2025-03-19 10:31:24 +0200

    aw

--- noaa/noaa/settings.py
+++ noaa/noaa/settings.py
@@ -76,10 +76,20 @@ WSGI_APPLICATION = 'noaa.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'noaa',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',  # or your PostgreSQL server address
+        'PORT': '5432',  # default PostgreSQL port
     }
 }
 
